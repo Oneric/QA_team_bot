31c3510e3798c117704780b7bca483b6742ade1b
Боровских Антон Евгеньевич <aborovskih@rtk-element.ru>
2024-04-07 10:11:18 +0700

    update final

--- Handlers/other_handlers.py
+++ Handlers/other_handlers.py
@@ -4,8 +4,8 @@ from aiogram.filters import CommandStart, Command
 
 from config_date.config import Config, load_config
 from keyboard.keyboards import create_key
+
 from lexicon_ru import LEXICON_MENU, LEXICON_TIME, LEXICON_CHAT_ID, LEXICON_MAIN_THREADS, LEXICON_QA_THREADS
-from time_message import send_message
 
 config: Config = load_config()
 
@@ -17,25 +17,43 @@ async def process_start_command(message: Message):
     await message.answer('Погнали.. ', reply_markup=keyboard_start)
 
 @router.message(Command("время"))
-async def deach(bot: Bot, message: Message):
-    await send_message(bot, LEXICON_CHAT_ID['OFFICE_TOMSK'], LEXICON_TIME['time'], LEXICON_MAIN_THREADS['General'])
+async def deach(message: Message, bot: Bot):
+    await bot.send_message(LEXICON_CHAT_ID['OFFICE_TOMSK'],
+                       LEXICON_TIME['time'],
+                       LEXICON_MAIN_THREADS['General']
+                       )
 
 @router.message(Command("душно"))
-async def deach(bot: Bot):
-    await send_message(bot, LEXICON_CHAT_ID['OFFICE_TOMSK'], LEXICON_TIME['window'], LEXICON_MAIN_THREADS['General'])
+async def deach(message: Message, bot: Bot):
+    await bot.send_message(LEXICON_CHAT_ID['OFFICE_TOMSK'],
+                       LEXICON_TIME['window'],
+                       LEXICON_MAIN_THREADS['General']
+                       )
 
 @router.message(Command("пока"))
-async def deach(bot: Bot):
-    await send_message(bot, LEXICON_CHAT_ID['OFFICE_TOMSK'], LEXICON_TIME['bye'], LEXICON_MAIN_THREADS['General'])
+async def deach(message: Message, bot: Bot):
+    await bot.send_message(LEXICON_CHAT_ID['OFFICE_TOMSK'],
+                       LEXICON_TIME['bye'],
+                       LEXICON_MAIN_THREADS['General']
+                       )
 
 @router.message(Command("предупреждение"))
-async def deach(bot: Bot):
-    await send_message(bot, LEXICON_CHAT_ID['OFFICE_TOMSK'], LEXICON_TIME['notification'], LEXICON_MAIN_THREADS['General'])
+async def deach(message: Message, bot: Bot):
+    await bot.send_message(LEXICON_CHAT_ID['OFFICE_TOMSK'],
+                       LEXICON_TIME['notification'],
+                       LEXICON_MAIN_THREADS['General']
+                       )
 
 @router.message(Command("зарядка"))
-async def deach(bot: Bot):
-    await send_message(bot, LEXICON_CHAT_ID['OFFICE_TOMSK'], LEXICON_TIME['charge'], LEXICON_MAIN_THREADS['General'])
+async def deach(message: Message, bot: Bot):
+    await bot.send_message(LEXICON_CHAT_ID['OFFICE_TOMSK'],
+                       LEXICON_TIME['charge'],
+                       LEXICON_MAIN_THREADS['General']
+                       )
 
 @router.message(Command("тест"))
-async def deach(bot: Bot):
-    await send_message(bot, LEXICON_CHAT_ID['QA_TEAM'], LEXICON_TIME['test'], LEXICON_QA_THREADS['Флудильня'])
+async def deach(message: Message, bot: Bot):
+    await bot.send_message(LEXICON_CHAT_ID['QA_TEAM'],
+                       LEXICON_TIME['test'],
+                       LEXICON_QA_THREADS['Флудильня']
+                       )
--- time_message.py
+++ time_message.py
@@ -7,5 +7,5 @@ from config_date.config import Config, load_config
 config: Config = load_config()
 
 
-async def send_message(bot: Bot, chat_id: int, message: str, message_thread_id: int) -> object:
+async def send_message(bot: Bot, chat_id: int, message: str, message_thread_id: int):
     await bot.send_message(chat_id, message, message_thread_id)
